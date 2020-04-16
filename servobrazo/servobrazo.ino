08c9212f5624e3a84c1096e3626d903039463303
Dave16E40 <jhondave_kun@hotmail.com>
2020-04-16 15:06:58 -0500

    brazo
    brazo robotico esferico

--- servobrazo/servobrazo.ino
+++ servobrazo/servobrazo.ino
@@ -6,18 +6,15 @@ Servo servo_motor;
 int potpin = 0;
 int val;
 
-
-
 void setup()
 {
- 	servo_motor.attach(3);
-  
+ 	servomotor.attach(3);  
 }
 
 void loop()
 {
 	val = analogRead(potpin);
   	val = map(val, 0 , 1023, 0 , 180);
-  	servo_motor.write(val);
+  	servomotor.write(val);
   	delay(15);
 }
